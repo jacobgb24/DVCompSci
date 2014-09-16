48c7fcbfbdda88c678f2df13a9e5a77312d9bf04
jacobgb24 <jacobgb24@yahoo.com>
2014-09-15 21:56:34 -0700

    Re arranging folders fix for DerivativeGui

--- Projects/DerivativeGui.java
+++ Projects/DerivativeGui.java
@@ -1,6 +1,6 @@
 public class DerivativeGui extends javax.swing.JFrame {
 
-    public Derivative() {
+    public DerivativeGui() {
         initComponents();
     }
 
@@ -125,7 +125,7 @@ public class DerivativeGui extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Derivative().setVisible(true);
+                new DerivativeGui().setVisible(true);
             }
         });
     }
@@ -141,3 +141,31 @@ public class DerivativeGui extends javax.swing.JFrame {
     private javax.swing.JLabel originalLabel;
     // End of variables declaration//GEN-END:variables
 }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+ 
+        });
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
