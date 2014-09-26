e870b4847a91af641676a0a7cc2c2f86d8d30e25
trentc98 <tcoppa98@gmail.com>
2014-09-25 22:46:43 -0700

    Line grapher
    Supposed to graph, but I can't get the stupid slope to reference.

---
+++ Projects/Graphing.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Graphing {
+   
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                argConfig();
+				createAndShowGUI(); 
+            }
+        });
+    }
+
+	public static void argConfig(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input the slope: ");
+		double slopeIn = sc.nextDouble();
+		System.out.print("Input the y-intercept: ");
+		double yInt = sc.nextDouble();
+		}
+	
+    private static void createAndShowGUI() {
+        System.out.println("Created GUI on EDT? "+
+        SwingUtilities.isEventDispatchThread());
+        JFrame f = new JFrame("Swing Paint Demo");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(new GraphPanel());
+        f.pack();
+        f.setVisible(true);
+		}
+}
+
+class GraphPanel extends JPanel {
+
+   public GraphPanel() {
+        setBorder(BorderFactory.createLineBorder(Color.black));
+		setBackground(Color.white);
+    }
+    
+
+    public Dimension getPreferredSize() {
+        return new Dimension(250,200);
+    }
+    
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);       
+		g.drawString("Please work",1,20);
+		g.drawString("Slope is: ", 1, 40);
+    }  
+}
