a7440ef18acf6823408cdbff53aa06aa8c788566
trentc98 <tcoppa98@gmail.com>
2014-09-27 17:21:31 -0700

    Graphs a line
    Added axes

--- Projects/Graphing.java
+++ Projects/Graphing.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Graphing {	//Setup class, prepares the GUI
    
-   static double slopeIn; //ALlows variables to be shared between methods
+   static double slopeIn; //Allows variables to be shared between methods
    static double yIntIn;
    
     public static void main(String[] args) {	//runs argConfig and then GUI methods
@@ -49,22 +49,36 @@ public class Graphing {	//Setup class, prepares the GUI
 class GraphPanel extends JPanel {	//GUI class, creates graph
 
 	Graphing test = new Graphing();	//Constructor for previous class
-	int slope = (int)test.getSlope();
+	double slope = test.getSlope();
 	int yInt = (int)test.getIntercept();
+	
+	int yIntNew = 500-yInt;
+	int slopePoint = (int)slope*500+yInt;
 
    public GraphPanel() {
-        setBorder(BorderFactory.createLineBorder(Color.black));
+       // setBorder(BorderFactory.createLineBorder(Color.black));
 		setBackground(Color.white);
     }
     
 
     public Dimension getPreferredSize() {
-        return new Dimension(250,200);
+        return new Dimension(520,500);
     }
     
     protected void paintComponent(Graphics g) {
 		super.paintComponent(g);       
-		g.drawString("Slope is: "+slope+"y-int is: "+yInt, 1, 20);
-		g.drawLine(0,200-yInt, 250, 200-slope*250); //Line graph
+		g.drawString("Slope is: "+slope+" y-int is: "+yInt, 21, 20);
+		g.drawLine(20, 0, 20, 500); //Y-axis
+		g.drawLine(0, 480, 500, 480); //X-axis
+		for(int i=0; i<=500; i++){
+			if(i%20==0){
+				g.drawLine(i, 485, i, 475); //X-axis markers
+				g.drawLine(15, i-20, 25, i-20);} //Y-axis markers
+			if(i%40==0){
+				g.drawString(""+i, i+13, 495);
+				g.drawString(""+(500-i), 0, i-15);}
+		}
+		
+		g.drawLine(0,yIntNew, 500, 500-slopePoint); //Line
     }  
 }
