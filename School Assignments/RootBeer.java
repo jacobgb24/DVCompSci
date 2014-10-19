20472b6c8bdbddfd99e3c2430b4db42bf2faea7e
trentc98 <tcoppa98@gmail.com>
2014-10-19 01:01:14 -0700

    Added audio and cleaned up
    The variables now have intuitive names. A burp plays once it reaches zero.
    THROWS 3 WARNINGS WHEN COMPILING, BUT IT WORKS FINE!

--- School Assignments/RootBeer.java
+++ School Assignments/RootBeer.java
@@ -1,21 +1,23 @@
 import java.util.*;
+import sun.audio.*;
+import java.io.*;
 
 public class RootBeer
 {
 	static String cont="yes";
-	static int intCount, finalCount, count=10;
-	public static void main(String[] args) throws InterruptedException
-	{
+	static int beerCount, initCount, delay=10;
+	public static void main(String[] args) throws InterruptedException{
+	
 		Scanner in = new Scanner(System.in);
 		System.out.println("How many bottles of Root Beer do you have?");
-		intCount=finalCount=in.nextInt();
+		beerCount=initCount=in.nextInt();
 		while(cont.equalsIgnoreCase("yes") || cont.equalsIgnoreCase("y")) {
 
-			while(intCount>=3) { //loop for 99-3
+			while(beerCount>=3) { //loop for 99-3
 				Chorus();
 				
-				Thread.sleep(count*5);
-				count++;
+				Thread.sleep(delay*5);
+				delay++;
 			}
 
 		//Ending for last two statements
@@ -23,26 +25,38 @@ public class RootBeer
 		System.out.println("2 bottles of root beer");
 		System.out.println("take one down pass it around,");
 		System.out.println("only 1 bottle of root beer on the wall\n");
-		Thread.sleep(count*5);
+		Thread.sleep(delay*5);
 		System.out.println("only 1 bottle of root beer on the wall,");
 		System.out.println("only 1 bottle of root beer");
 		System.out.println("take it down pass it around,");
 		System.out.println("no more bottles of root beer on the wall\n");
-		Thread.sleep(count*5);
+		Thread.sleep(delay*5);
 		System.out.println("no more bottles of root beer on the wall");
 		System.out.println("go to the store get some more,");
-		System.out.println( finalCount+" bottles of root beer on the wall");
+		System.out.println( initCount+" bottles of root beer on the wall");
+		Sound();
 		Thread.sleep(1000);
 		System.out.println("\n\nRestart?(type yes)");
 		cont=in.next();
-		intCount=99;
+		beerCount=initCount;
+		}
+	}	
+	private static void Chorus(){
+		System.out.println(beerCount + " bottles of root beer on the wall,");
+		System.out.println(beerCount + " bottles of root beer");
+		System.out.println("take one down pass it around,");
+		beerCount--;
+		System.out.println(beerCount + " bottles of root beer on the wall,\n");
 	}
-}	
-	public static void Chorus() {
-			System.out.println(intCount + " bottles of root beer on the wall,");
-			System.out.println(intCount + " bottles of root beer");
-			System.out.println("take one down pass it around,");
-			intCount--;
-			System.out.println(intCount + " bottles of root beer on the wall,\n");
-	}		
+	
+	private static void Sound(){	//Ignore warnings when compiling, it works fine
+		try{
+		InputStream in = new FileInputStream("/Users/Trent/Desktop/CompSci/burp.wav");	//Path of the sound file
+		
+		AudioStream audioStream = new AudioStream(in);	//Constructs something
+		
+		AudioPlayer.player.start(audioStream);	//Plays the audio
+		}
+		catch(Exception e){}
+	}
 }
