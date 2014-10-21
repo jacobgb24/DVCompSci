2d25973dff3a56f0d6eb97769d4af1e1af447bfb
PotatoSwug <suchmsgmuchwow@hotmail.com>
2014-10-20 20:21:25 -0700

    Create NoobIntergral.java

---
+++ School Assignments/NoobIntergral.java
@@ -0,0 +1,26 @@
+// Cleanish version of intergral - JH
+import java.util.*;
+public class CurveArea
+{
+ public static void main (String[] args)
+ {
+  
+  double z=0, xy, x, y, area, farea=0, xi=0, xf=4.0;
+ for (double div=100000000; div>=0; div--)
+  {
+  
+  
+   x=(xf-xi)/100000000;
+   z += x;
+   xy = z;
+   y = 2*xy*xy;
+   area = x*y;
+   farea += area;
+   
+  }
+  
+ 	  System.out.println(farea);
+   
+ 	
+ }
+}
