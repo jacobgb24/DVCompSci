68c4dbf7e5af393d27b7490a027cba60e6fc08ab
jacobgb24 <jacobgb24@yahoo.com>
2014-10-20 20:46:32 -0700

    Allows Quadratic input

--- School Assignments/Integrals.java
+++ School Assignments/Integrals.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 public class Integrals{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		System.out.print("Please input a max x value: ");
+		
+		System.out.println("Please input the coefficients of a quadratic A(x^2)+Bx+C"); //taking coef of quad
+		double a = in.nextDouble();
+		double b = in.nextDouble();
+		double c = in.nextDouble();
+		
+		System.out.print("Please input a max x value: "); //domain
 		float domain= in.nextFloat();
 		
+		System.out.println("You entered: "+(int)a+"x^2+"+(int)b+"x+"+(int)c+" in the domain: [0,"+(int)domain+"].");
 		int x=0, max=1000000;
 		double section=max/domain, area=0, y;
 		
-		while(x<=max){
-			y=2*Math.pow(x/section, 2);
+		while(x<=max){ //calculation
+			y=(a*Math.pow(x/section, 2))+(b*x/section)+c;
 			area+=y/section;
 			x++;
 		}
