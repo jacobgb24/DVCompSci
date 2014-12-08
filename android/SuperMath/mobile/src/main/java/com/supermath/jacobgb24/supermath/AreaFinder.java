727b4420e385835799963c22f5452d93b1ee529e
jacobgb24 <jacobgb24@yahoo.com>
2014-12-07 20:30:10 -0800

    Added AreaFinder v0.2
    AreaFinder now working; fixed crash when button is unexpectedly pressed;
    added xml for GuessNum

--- android/SuperMath/mobile/src/main/java/com/supermath/jacobgb24/supermath/AreaFinder.java
+++ android/SuperMath/mobile/src/main/java/com/supermath/jacobgb24/supermath/AreaFinder.java
@@ -1,7 +1,96 @@
 package com.supermath.jacobgb24.supermath;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
 /**
  * Created by jacob_000 on 12/5/2014.
  */
-public class AreaFinder {
+public class AreaFinder extends ActionBarActivity {
+    private RadioGroup shape;
+    private static double A1, A2, area;
+    private static EditText Anum1, Anum2;
+    private static Button calculate;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aflayout);
+        Math();
+    }
+    public void Math() {
+        shape = (RadioGroup) findViewById(R.id.AFchoice);
+        calculate = (Button) findViewById(R.id.Calculateaf);
+        Anum1 = (EditText) findViewById(R.id.Anum1);
+        Anum2 = (EditText) findViewById(R.id.Anum2);
+
+        shape.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rectB)
+                    RectMath();
+                else if (checkedId == R.id.triB)
+                    TriMath();
+                else
+                    CircMath();
+            }
+        });
+    }
+
+    public void RectMath() {
+        Anum1.setHint("Length");
+        Anum2.setVisibility(View.VISIBLE);
+        Anum2.setHint("Width");
+        button();
+    }
+    public void TriMath() {
+        Anum1.setHint("Base");
+        Anum2.setVisibility(View.VISIBLE);
+        Anum2.setHint("Height");
+        button();
+    }
+    public void CircMath() {
+        Anum1.setHint("Radius");
+        Anum2.setVisibility(View.GONE);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    A1 = Double.parseDouble(Anum1.getText().toString());
+                    area = Math.pow(A1, 2) * Math.PI;
+                    DecimalFormat fmt = new DecimalFormat("########.###");
+                    String fanswer = fmt.format(area);
+                    TextView textView = (TextView) findViewById(R.id.Answeraf);
+                    textView.setText(String.valueOf(fanswer));
+                    area = 0;
+                } catch (Exception e) {
+                }
+            }
+        });
+    }
+    public void button() {
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    A1 = Double.parseDouble(Anum1.getText().toString());
+                    A2 = Double.parseDouble(Anum2.getText().toString());
+                    area = A1 * A2;
+                    DecimalFormat fmt = new DecimalFormat("########.###");
+                    String fanswer = fmt.format(area);
+                    TextView textView = (TextView) findViewById(R.id.Answeraf);
+                    textView.setText(String.valueOf(fanswer));
+                    area=0;
+                }catch(Exception e) { }
+            }
+        });
+    }
 }
--- android/SuperMath/mobile/src/main/java/com/supermath/jacobgb24/supermath/Integral.java
+++ android/SuperMath/mobile/src/main/java/com/supermath/jacobgb24/supermath/Integral.java
@@ -14,9 +14,10 @@ import java.text.DecimalFormat;
  */
 public class Integral extends Activity {
     static double Ain, Bin, Cin, ADin, BDin;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gnlayout);
+        setContentView(R.layout.ilayout);
         final EditText Aquad = (EditText) findViewById(R.id.Ain);
         final EditText Bquad = (EditText) findViewById(R.id.Bin);
         final EditText Cquad = (EditText) findViewById(R.id.Cin);
@@ -26,29 +27,31 @@ public class Integral extends Activity {
         Button calculate = (Button) findViewById(R.id.Calculate);
         calculate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Ain = Double.parseDouble(Aquad.getText().toString());
-                Bin = Double.parseDouble(Bquad.getText().toString());
-                Cin = Double.parseDouble(Cquad.getText().toString());
-                ADin = Double.parseDouble(Adom.getText().toString());
-                BDin = Double.parseDouble(Bdom.getText().toString());
-                Math();
+                try {
+                    Ain = Double.parseDouble(Aquad.getText().toString());
+                    Bin = Double.parseDouble(Bquad.getText().toString());
+                    Cin = Double.parseDouble(Cquad.getText().toString());
+                    ADin = Double.parseDouble(Adom.getText().toString());
+                    BDin = Double.parseDouble(Bdom.getText().toString());
+                    Math();
+                }catch (Exception e) { }
             }
         });
     }
-    private void Math(){
 
-        final int MAX=100000;
-        double sectionB=MAX/BDin, sectionA=MAX/ADin, yb, ya, area=0;
-        for(int i=0; i<MAX; i++ ) {
-              yb=(Ain*Math.pow(i/sectionB, 2))+(Bin*i/sectionB)+Cin;
-              ya=(Ain*Math.pow(i/sectionA, 2))+(Bin*i/sectionA)+Cin;
-              area+=yb/sectionB;
-              area-=ya/sectionA;
-             }
-        DecimalFormat fmt = new DecimalFormat("###,###,###,###,###.###");
-        String finArea = fmt.format(area);
-        TextView answer = (TextView) findViewById(R.id.Answer);
-        answer.setText(String.valueOf(finArea));
-        area=0;
+    private void Math() {
+            final int MAX = 100000;
+            double sectionB = MAX / BDin, sectionA = MAX / ADin, yb, ya, area = 0;
+            for (int i = 0; i < MAX; i++) {
+                yb = (Ain * Math.pow(i / sectionB, 2)) + (Bin * i / sectionB) + Cin;
+                ya = (Ain * Math.pow(i / sectionA, 2)) + (Bin * i / sectionA) + Cin;
+                area += yb / sectionB;
+                area -= ya / sectionA;
+            }
+            DecimalFormat fmt = new DecimalFormat("###,###,###,###,###.###");
+            String finArea = fmt.format(area);
+            TextView answer = (TextView) findViewById(R.id.Answer);
+            answer.setText(String.valueOf(finArea));
+            area = 0;
     }
 }
